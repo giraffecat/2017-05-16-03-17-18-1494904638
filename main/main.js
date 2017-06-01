151a8a5634a85d79b918229835c73b4205cbb692
giraffecat <1018997102@qq.com>
2017-06-01 11:54:16 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,10 +1,5 @@
 // Write your cade below:
 module.exports = function main() {
-  scanener  scanner=new scanner(system.in);
-var a,b,z;
-  a=scanner.nextlnt();
-  b=scanner.nextlnt();
-  z=a%b;
-System.out.println("please pay: " + z);
-  
+ var num=9;
+  console.log(num%4);
 };
