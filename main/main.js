9556fec37280332c1b42c99a4d7746c985bdc4b1
giraffecat <1018997102@qq.com>
2017-06-01 11:00:55 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 // Write your cade below:
 module.exports = function main() {
+  int a，b，t；
+  scanf（"input number :"，&a）；
+  scanf（"input number:"，&b）；
+  t=a%b；
+  printf（"the number is:"，t）
+  
   
 };
